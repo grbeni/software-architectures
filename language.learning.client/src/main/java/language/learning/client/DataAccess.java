8a8884a72c681654566755833da8687a95aaf770
Bence Graics <grbeni@gmail.com>
2017-11-21 20:02:20 +0100

    Client creation refactor.

--- language.learning.client/src/main/java/language/learning/client/DataAccess.java
+++ language.learning.client/src/main/java/language/learning/client/DataAccess.java
@@ -17,83 +17,77 @@ import language.learning.user.User;
 
 public class DataAccess implements IExerciseManager, ILearning, IUserManager {
 
-	private IExerciseManager exerciseManager;
-	private ILearning learning;
-	private IUserManager userManager;
-
-	public DataAccess() {
-
-	}
+	public DataAccess() {}
 
 	@Override
 	public User logIn(String username) {
-		userManager = createClient(IUserManager.class);
+		IUserManager userManager = createClient(IUserManager.class);
 		return userManager.logIn(username);
 	}
 
 	@Override
 	public boolean addUser(User user) {
-		userManager = createClient(IUserManager.class);
+		IUserManager userManager = createClient(IUserManager.class);
 		return userManager.addUser(user);
 	}
 
 	@Override
 	public boolean deleteUser(String username) {
-		userManager = createClient(IUserManager.class);
+		IUserManager userManager = createClient(IUserManager.class);
 		return userManager.deleteUser(username);
 	}
 
 	@Override
 	public SentenceExercises getExercises(String type, int count) {
-		learning = createClient(ILearning.class);
+		ILearning learning = createClient(ILearning.class);
 		return learning.getExercises(type, count);
 	}
 
 	@Override
 	public SentenceExercises getSentenceExercises(String userLevel, boolean equals, int count) {
-		learning = createClient(ILearning.class);
+		ILearning learning = createClient(ILearning.class);
 		return learning.getSentenceExercises(userLevel, equals, count);
 	}
 
 	@Override
 	public FourWordsExercises getWordExercises(String userLevel, boolean equals, int count) {
-		learning = createClient(ILearning.class);
+		ILearning learning = createClient(ILearning.class);
 		return learning.getWordExercises(userLevel, equals, count);
 	}
 
 	@Override
 	public ImageExercises getImageExercises(String userLevel, boolean equals, int count) {
-		learning = createClient(ILearning.class);
+		ILearning learning = createClient(ILearning.class);
 		return learning.getImageExercises(userLevel, equals, count);
 	}
 
 	@Override
 	public void updateUserScore(int score, User user) {
-		learning = createClient(ILearning.class);
+		ILearning learning = createClient(ILearning.class);
 		learning.updateUserScore(score, user);
 	}
 
 	@Override
 	public void updateUserLevel(String userLevel, User user) {
-		learning = createClient(ILearning.class);
+		ILearning learning = createClient(ILearning.class);
 		learning.updateUserLevel(userLevel, user);
 	}
 
 	@Override
 	public boolean addExercise(Exercise exercise, User user) {
-		exerciseManager = createClient(IExerciseManager.class);
+		IExerciseManager exerciseManager = createClient(IExerciseManager.class);
 		return exerciseManager.addExercise(exercise, user);
 	}
 
 	@Override
 	public boolean deleteExercise(Exercise exercise, User user) {
-		exerciseManager = createClient(IExerciseManager.class);
+		IExerciseManager exerciseManager = createClient(IExerciseManager.class);
 		return exerciseManager.deleteExercise(exercise, user);
 	}
 
 	@Override
 	public boolean updateExerciseWithImage() {
-		exerciseManager = createClient(IExerciseManager.class);
+		IExerciseManager exerciseManager = createClient(IExerciseManager.class);
 		return exerciseManager.updateExerciseWithImage();
 	}
 

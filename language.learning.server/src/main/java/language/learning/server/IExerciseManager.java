320bb05efb501829727fd004d6aa12e6018ecb66
Bence Graics <grbeni@gmail.com>
2017-11-22 21:35:23 +0100

    Images added.

---
+++ language.learning.server/src/main/java/language/learning/server/IExerciseManager.java
@@ -0,0 +1,38 @@
+package language.learning.server;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import language.learning.exercise.Exercise;
+import language.learning.exercise.ExerciseWithImage;
+import language.learning.exercise.SentenceExercises;
+
+@Path("exercise")
+public interface IExerciseManager {
+	
+	@POST
+	@Path("add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	void addExercise(@QueryParam("username") String username, Exercise exercise);
+	
+	@POST
+	@Path("image")
+	@Consumes(MediaType.APPLICATION_JSON)
+	void addExercise(@QueryParam("username") String username, ExerciseWithImage exercise);
+	
+	@DELETE
+	@Path("delete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	void deleteExercise(@QueryParam("username") String username, Exercise exercise);
+	
+	@GET
+	@Path("list")
+	@Consumes(MediaType.APPLICATION_JSON)
+	SentenceExercises listExercises();
+	
+}

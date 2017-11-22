cd8fd5ff6d8aa6407dee7361420882b3b1a36886
Verbőczy Kristóf <kristof.verboczy@gmail.com>
2017-11-22 22:52:15 +0100

    Fix

--- language.learning.server/src/main/java/language/learning/server/Learning.java
+++ language.learning.server/src/main/java/language/learning/server/Learning.java
@@ -117,6 +117,8 @@ public class Learning implements ILearning {
 
 		FourWordsExercises ex = getRandomNCountExerciseFourWords(exerciseList, count);
 
+		log.error("4w list: " + ex);
+		
 		return ex;
 	}
 
